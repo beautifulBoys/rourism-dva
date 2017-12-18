b871e1f5c8c3b4c7b15ded83fc0eaa42bc1b95a8
beautifulBoys <1298947916@qq.com>
2017-12-18 13:55:51 +0800

    error

--- src/models/friend.js
+++ src/models/friend.js
@@ -1,11 +1,50 @@
 
+import Cookies from 'js-cookie';
+import { myFriendAjax, deleteFriendAjax } from '../api/index.js';
+import { notification, message } from 'antd';
+
 export default {
-  namespace: 'choice_img',
+  namespace: 'friend',
   state: {
     list: []
   },
   effects: {
+    *getData ({}, { call, put, select }) {
+      const result = yield call(myFriendAjax);
+      if (result.code === 200) {
+        yield put({
+          type: 'setDataList',
+          list: result.data.list
+        });
+      } else {
+        message.error(result.message);
+      }
+    },
+    *deleteFriendEvent ({data}, { call, put, select }) {
+      const result = yield call(deleteFriendAjax, {id: data.id});
+      if (result.code === 200) {
+        if (data.cbb) data.cbb();
+        message.success(result.message);
+        yield put({
+          type: 'setDataList',
+          list: result.data.list
+        });
+      } else {
+        message.error(result.message);
+      }
+    }
   },
   reducers: {
+    setDataList (state, { list }) {
+      let arr = [];
+      list.map((item, index) => {
+        item.key = index;
+        arr.push(item);
+      });
+      return {
+        ...state,
+        list: [...arr]
+      };
+    }
   },
 };
